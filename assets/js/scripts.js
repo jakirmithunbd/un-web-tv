a995eb6686f01a3a85a3c732efa56696c662baf2
unknown <jakirmithun>
2020-01-21 15:07:20 +0600

    4 page done

---
+++ assets/js/scripts.js
@@ -0,0 +1,130 @@
+
+(function($){
+    var ua = window.navigator.userAgent;
+    var isIE = /MSIE|Trident/.test(ua);
+
+    if ( !isIE ) {
+        //IE specific code goes here
+        "use strict";
+    }
+
+      /*** Sidr Menu */
+    // $('.navbar-toggle').sidr({
+    //     name: 'sidr-main',
+    //     side: 'right',
+    //     source: '#sidr',
+    //     displace: false,
+    //     renaming: false
+    // });
+
+    $('.navbar-toggle').on('click', function(){
+        $(this).toggleClass('in');
+    });
+
+    /*** Header height = gutter height */
+    function headersetGutterHeight(){
+        var footer = document.querySelector('.header'),
+            gutter = document.querySelector('.header_gutter');
+            gutter.style.height = footer.offsetHeight + 'px';
+    }
+
+    window.onload = headersetGutterHeight;
+    window.onresize = headersetGutterHeight;
+
+    $("document").on("click",function(e) { $.sidr('close','sidr-main'); });
+
+    $('.closeMenu').on('click', function(){
+        $.sidr('close', 'sidr-main');
+    });
+
+    /*** Sticky header */
+    $(window).scroll(function() {
+
+        if ($(window).scrollTop() > 0) {
+          $(".header").addClass("sticky");
+        } 
+        else {
+          $(".header").removeClass("sticky");
+        }
+    });
+
+     // Mobile package Silder
+    $(".live-slider-wrapper").slick({
+        // dots: true,
+        infinite: false,
+        draggable: true,
+        slidesToShow: 4,
+        // autoplay: true,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+        {
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+     // Mobile package Silder
+    $(".slider-wrapper").slick({
+        // dots: true,
+        infinite: false,
+        draggable: true,
+        slidesToShow: 4,
+        // autoplay: true,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+        {
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+    $('a[href*="#"]').on('click', function(e) {
+      e.preventDefault()
+
+      $('html, body').animate(
+        {
+          scrollTop: $($(this).attr('href')).offset().top - 120,
+        },
+        500,
+      )
+    });
+
+})(jQuery);
