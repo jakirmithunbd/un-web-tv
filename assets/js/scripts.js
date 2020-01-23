0f77a8ffae3c531fb56221f9c094a32b307102f1
unknown <jakirmithun>
2020-01-23 22:10:38 +0600

    category page wrking

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -117,14 +117,16 @@
     });
 
     $('a[href*="#"]').on('click', function(e) {
-      e.preventDefault()
+      e.preventDefault();
 
-      $('html, body').animate(
-        {
+      $('html, body').animate({
           scrollTop: $($(this).attr('href')).offset().top - 120,
-        },
-        500,
-      )
+        }, 500);
+    });
+
+    $('.hidden-cat-item').on('click', function(){
+      $(this).parent().toggleClass('ShowSubCate');
+
     });
 
 })(jQuery);
